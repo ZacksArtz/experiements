b017a075af9bc8f69dade1fb51e1a4efb9576a7b
Zack <zack@Zacks-MacBook-Pro.local>
2018-01-31 21:14:12 -1000

    xdd

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const https = require('https')
 
 app.use(express.static(__dirname + '/public'));
-
-
-
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     res.render('pages/index')
-});
+})
+
+// pings server every 15 minutes to prevent dynos from sleeping
+setInterval(() => {
+    https.get('https://zacharymyers.com');
+}, 900000);
 
 app.listen(port, () => {
     // will echo 'Our app is running on http://localhost:5000 when run locally'
